e369dd6d7b9f833468f871948fcfcbac53fe27b5
Tyler-McIntyre <tmcintyre802@gmail.com>
2024-01-28 20:17:18 -0500

    move code to try block

--- GlobalWeatherApi/Controllers/WeatherController.cs
+++ GlobalWeatherApi/Controllers/WeatherController.cs
@@ -1,6 +1,7 @@
 using GlobalWeatherApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace GlobalWeatherApi.Controllers
 {
@@ -27,7 +28,7 @@ namespace GlobalWeatherApi.Controllers
         /// <returns>General weather info</returns>
         /// <exception cref="Exception"></exception>
         [HttpGet(Name = "GetWeatherInfo")]
-        public async Task<WeatherInfo> Get(float latitude = 35.7796f, float longitude = 78.6382f)
+        public async Task<ActionResult<WeatherInfo>> Get(float latitude = 35.7796f, float longitude = 78.6382f)
         {
             _logger.LogInformation("Received weather request for Latitude: {Latitude}, Longitude: {Longitude}", latitude, longitude);
 
@@ -35,29 +36,27 @@ namespace GlobalWeatherApi.Controllers
             string queryString = $"?lat={latitude}&lon={longitude}&appid={_weatherApiSettings.ApiKey}";
             string url = _weatherApiSettings.BaseUrl + _weatherApiSettings.Endpoint + queryString;
 
-            HttpResponseMessage response;
             try
             {
-                // Make http request
+                // Make request
                 HttpClient httpClient = new();
-                response = await httpClient.GetAsync(url);
+                HttpResponseMessage response = await httpClient.GetAsync(url);
                 response.EnsureSuccessStatusCode();
+
+                string responseContent = await response.Content.ReadAsStringAsync();
+                WeatherInfo? weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(responseContent);
+
+                // Deserialization failed
+                if (weatherInfo is null) return StatusCode(500);
+
+                weatherInfo.ClassifyTemperature();
+                return weatherInfo;
             } 
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error retrieving weather data for Latitude");
+                _logger.LogError(ex, "Internal Server Error");
                 throw;
             }
-
-            // Read content from response and deserialize to WeatherInfo object
-            string responseContent = await response.Content.ReadAsStringAsync() ?? throw new Exception("Failed to retrieve weather info");
-
-            WeatherInfo weatherInfo = JsonConvert.DeserializeObject<WeatherInfo>(responseContent)!;
-            weatherInfo.ClassifyTemperature();
-
-            _logger.LogInformation("Weather data retrieved successfully");
-
-            return weatherInfo;
         }
     }
 }
